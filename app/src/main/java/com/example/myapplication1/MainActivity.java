d0cf59aafb5808cd862bccca659eca4f6f22c780
ahlem123456 <ahlemfrih97@gmail.com>
2020-10-10 09:44:06 +0100

    initial commit

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Nullable
+    @Override
+    public ActionBar getActionBar() {
+        return super.getActionBar();
     }
 }
